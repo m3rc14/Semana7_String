3f565ec36aba53aef975eee4c2612dc6c3d52acb
m3rc14 <87401320+m3rc14@users.noreply.github.com>
2021-12-23 20:03:11 -0300

    Add files via upload

---
+++ Semana7_Q1.js
@@ -0,0 +1,27 @@
+function myFunction() {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Pastel");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Coxinha");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Misto quente");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Lanche-Bem");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Mini-pizza");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+
+}
